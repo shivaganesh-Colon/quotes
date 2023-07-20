600fc25a2ef087e5b067cc2ef7276bbdab5f6178
shiva ganesh <shivaganeshyadav@gmail.com>
2023-07-20 13:01:29 +0530

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.css';
 function App() {
   return (
     <div className="App">
-      <h1>Start Pro</h1>
+      <p className='text-uppercase'>Start Shiva ganesh</p>
     </div>
   );
 }
