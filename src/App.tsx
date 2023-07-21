2d760d89af267b865241d816eb3c6a904a9f0048
shiva ganesh <shivaganeshyadav@gmail.com>
2023-07-21 23:58:13 +0530

    amplify files

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import awsConfig from './aws-exports'
+
 function App() {
   return (
     <div className="App">
